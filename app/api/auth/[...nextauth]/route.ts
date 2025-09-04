d021769246184d5e53e0aa2d7883ccb54ee24950
Ash Hammond <ash_hammond@fastmail.com>
2025-09-04 08:43:55 -0500

    auth

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,13 +1,6 @@
+import { config } from "@/helpers/auth"
 import NextAuth from "next-auth"
-import DiscordProvider from 'next-auth/providers/discord'
 
-const handler = NextAuth({
-    providers: [
-        DiscordProvider({
-            clientId: process.env.DISCORD_CLIENT_ID!,
-            clientSecret: process.env.DISCORD_CLIENT_SECRET!,
-        })
-    ]
-})
+const handler = NextAuth(config)
 
 export { handler as GET, handler as POST }
---
+++ helpers/auth.ts
@@ -0,0 +1,41 @@
+import type {
+    GetServerSidePropsContext,
+    NextApiRequest,
+    NextApiResponse,
+} from "next"
+import DiscordProvider from 'next-auth/providers/discord'
+import type { DefaultSession, NextAuthOptions } from "next-auth"
+import { getServerSession } from "next-auth"
+
+export type DiscordUser = {
+    id: string | null | undefined
+} & DefaultSession['user']
+
+export const config = {
+    providers: [
+        DiscordProvider({
+            clientId: process.env.DISCORD_CLIENT_ID!,
+            clientSecret: process.env.DISCORD_CLIENT_SECRET!,
+        })
+    ],
+    callbacks: {
+        async session({ session, token, user }) {
+            if (token) {
+                if (token?.picture?.includes("discord")) {
+                    (session!.user! as DiscordUser).id = token.sub;
+                }
+            }
+            return session;
+        },
+    },
+} satisfies NextAuthOptions
+
+// Use it in server contexts
+export function auth(
+    ...args:
+        | [GetServerSidePropsContext["req"], GetServerSidePropsContext["res"]]
+        | [NextApiRequest, NextApiResponse]
+        | []
+) {
+    return getServerSession(...args, config)
+}
