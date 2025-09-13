928330b6b9b3670c5f52c3dab0626ad369e99ad6
Ash Hammond <ash_hammond@fastmail.com>
2025-09-13 11:48:51 -0500

    crt page

---
+++ app/crt/[slug]/page.tsx
@@ -0,0 +1,23 @@
+import prisma from "@/client"
+import NotFound from "@/components/not-found"
+
+export default async function BlogPostPage({
+    params,
+}: {
+    params: Promise<{ slug: string }>
+}) {
+    const { slug } = await params
+    const crt = await prisma.cRT.findFirst({
+        where: {
+            id: parseInt(slug)
+        }
+    })
+
+    if (crt == null) return <NotFound />
+
+    return (
+        <div>
+            <h1>{crt.model}</h1>
+        </div>
+    )
+}
