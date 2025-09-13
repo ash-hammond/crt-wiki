2eb533466a14729373852931f31b6778bc5c91bb
Ash Hammond <ash_hammond@fastmail.com>
2025-09-13 10:19:22 -0500

    S3 & Simplify home

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,16 @@
+import { PropsWithChildren } from "react";
+
+
 export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <h1>Community CRT Wiki</h1>
-        <ol className="list-decimal">
-          <li><a className="underline" href="https://discord.gg/FGwpEpQGKD" target="_blank">Join our discord</a></li>
-          <li>
-            <a className="underline" href="https://docs.google.com/document/d/1fm_1GCBCUSzVkF3bIC8yHQ1HBPrJ3-PUr_1Qhzi-l9Q/edit?tab=t.0" target="_blank">Copy the article submission template</a>
-          </li>
-          <li>
-            <a className="underline" href="https://forms.gle/6pDeEcA1TzztCZQr9" target="_blank">Submit your article to the Google Form</a>
-          </li>
-        </ol>
+        <a className="underline" href="/submit">Submit CRT</a>
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
         <a className="underline" href="https://github.com/ash-hammond/crt-wiki" target="_blank">GitHub Repository</a>
+        <a className="underline" href="https://discord.gg/FGwpEpQGKD" target="_blank">Join our discord</a>
       </footer>
     </div>
   );
