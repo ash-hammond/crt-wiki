9e578087a474d7388e41a32f821657873c19f2a6
Ash Hammond <ash_hammond@fastmail.com>
2025-09-14 13:12:27 -0500

    Image upload

--- app/crt/submit/actions.ts
+++ app/crt/submit/actions.ts
@@ -7,15 +7,23 @@ import * as z from "zod";
 export async function submitCRT(data: CRTSubmission) {
     CRTSubmissionSchema.parse(data)
     if (!verifyAdmin()) return false
-    const images = await data.images.map(async (image) => ({
+    const images = await Promise.all(data.images.map(async (image) => ({
         description: image.name,
-        data: await image.arrayBuffer()
-    }))
+        data: Buffer.from(await image.arrayBuffer())
+    })))
     const shallowData: any = { ...data }
     shallowData.images = undefined
 
     const crt = await prisma.cRT.create({
-        data: shallowData as Omit<CRTSubmission, "images">
+        data: {
+            ...shallowData as Omit<CRTSubmission, "images">,
+            images: {
+                create: images
+            }
+        },
+        include: {
+            images: true
+        }
     })
 
     return { success: true };
